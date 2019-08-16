ba649bda5636070644f6f6b47c768eff04532aa5
luxiaohua <luxiaohua@jielema.com>
2019-08-16 17:06:49 +0800

    使用feign调用订单服务

---
+++ dohko-eureka-client-feign/src/main/java/com/dohko/feign/manager/FeignOrderService.java
@@ -0,0 +1,19 @@
+package com.dohko.feign.manager;
+
+import com.dohko.feign.dto.Order;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * @Description feign调用订单服务
+ * @Author luxiaohua
+ * @Date 2019/8/16
+ */
+
+@FeignClient(value = "DOHKO-EUREKA-CLIENT-ORDER-SERVICE")
+public interface FeignOrderService {
+
+    @GetMapping("/orders/{id}")
+    Order getById(@PathVariable Long id);
+}
